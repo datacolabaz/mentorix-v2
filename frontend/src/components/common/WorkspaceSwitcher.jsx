0738de988001549d9215d631d151ac86cb2a0c6a
Telman Abdulla <telmanabdulladev@gmail.com>
2026-09-17 17:26:23 +0400

    Fix missing Link import in WorkspaceSwitcher

--- frontend/src/components/common/WorkspaceSwitcher.jsx
+++ frontend/src/components/common/WorkspaceSwitcher.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import useAuthStore from '../../hooks/useAuth'
 import useUiStore from '../../hooks/useUi'
